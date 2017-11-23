a0d37544dd297c3527eea820bfe14e962c2b0d6a
Jack English <16035232@students.southwales.ac.uk>
2017-11-23 10:31:49 +0000

    set up

--- BayView/Form1.cs
+++ BayView/Form1.cs
@@ -16,5 +16,15 @@ namespace BayView
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
