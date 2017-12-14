5d0ef95e597eeec3fea6c8fa036f79489e753a5e
Joshua Rowlands <15582400@students.southwales.ac.uk>
2017-12-14 10:11:30 +0000

    Button

--- BayView/Form1.Designer.cs
+++ BayView/Form1.Designer.cs
@@ -41,16 +41,16 @@
             this.tb_newpassword = new System.Windows.Forms.TextBox();
             this.btn_submit2 = new System.Windows.Forms.Button();
             this.btn_cancel = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(38, 63);
+            this.label1.Location = new System.Drawing.Point(22, 36);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(146, 33);
+            this.label1.Size = new System.Drawing.Size(130, 26);
             this.label1.TabIndex = 0;
             this.label1.Text = "Username";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -157,27 +157,27 @@
             this.btn_cancel.Text = "Cancel";
             this.btn_cancel.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // button2
             // 
-            this.textBox1.Location = new System.Drawing.Point(201, 63);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(380, 56);
-            this.textBox1.TabIndex = 1;
+            this.button2.Location = new System.Drawing.Point(432, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(268, 139);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1032, 563);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(885, 412);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "Form1";
             this.Text = "BayView Hotel";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -195,7 +195,7 @@
         private System.Windows.Forms.TextBox tb_newpassword;
         private System.Windows.Forms.Button btn_submit2;
         private System.Windows.Forms.Button btn_cancel;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
